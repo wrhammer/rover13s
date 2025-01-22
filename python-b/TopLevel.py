aa6bb183262639529f4baf61a3e43dd91f30f4f6
wrhammer <wrhammer@gmail.com>
2025-01-21 20:24:10 -0800

    Update TopLevel.py

--- python-b/TopLevel.py
+++ python-b/TopLevel.py
@@ -1,12 +1,20 @@
-#!/usr/bin/python
-#from linuxcnc import INTERP_EXECUTE_FINISH, INTERP_OK
+#   This is a component of LinuxCNC
+#   Copyright 2011, 2013, 2014 Dewey Garrett <dgarrett@panix.com>,
+#   Michael Haberler <git@mah.priv.at>, Norbert Schechner <nieson@web.de>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
 import remap
 
-def __init__(self,**words):
-    print len(words)," words passed"
-    for w in words:
-        print "%s: %s" % (w, words[w])
-
-    
-#def pi(self):
-#	return 3.1415926535897932
