c5a9f25db33c50c48c8891b145abbe9cbb41bd1c
Wade Hammerstrom <wrhammer@DaybreakCNC.lan>
2025-01-21 19:29:34 -0800

    hal and ini updates

--- Rover13s/python/tool_remap.py
+++
@@ -1,9 +0,0 @@
-import sys
-print("Python search paths:", sys.path)
-
-from stdglue import *
-import linuxcnc
-
-def test_remap(prolog, params):
-    print("Basic remap function called successfully.")
-    return
--- Rover13s/python/toplevel.py
+++
@@ -1,3 +0,0 @@
-import sys
-#sys.path.append("/home/wrhammer/linuxcnc/configs/Rover13s/python")
-import remap
